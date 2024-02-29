7322dafe94a6b020a3a9ea63f78a65431e85a2bf
Benjamin Saks <saksben2@gmail.com>
2024-02-28 19:50:55 -0700

    Fixed Issue 3 - put spaces in rev

--- src/ui.js
+++ src/ui.js
@@ -27,13 +27,26 @@ export const makeTable = async () => {
       const date = new Date(iso);
       return date.getHours() + ":" + date.getMinutes();
     }
+
+    //take the rev number and add a space every 3 integers
+    let moneyArr = [];
+    const rev = company[REVENUE_YTD_FIELD_NAME].toString();
+    let count = 0;
+    for (let i = rev.length - 1; i >= 0; i--) { 
+      moneyArr.unshift(rev[i]);
+      count++;
+      if (count % 3 == 0) {
+        moneyArr.unshift(" ")
+      }
+    }
+
     // let readableDate = company[CREATED_AT_FIELD_NAME].substring(11, 16); //creates universal readable time. May be too simple a solution
     row.push(
       company[COMPANY_NAME_FIELD_NAME],
       company[STATUS_FIELD_NAME],
       readableDate(), //returns readable time instead of api's company[CREATED_AT_FIELD_NAME]
       // readableDate, //returns readable time instead of api's company[CREATED_AT_FIELD_NAME]
-      company[REVENUE_YTD_FIELD_NAME],
+      moneyArr.join(''), //returns readable number instead of api's company[REVENUE_YTD_FIELD_NAME]
       company[ACCOUNT_EXECUTIVE_FIELD_NAME]
     );
     companiesToDisplay.push(row);
